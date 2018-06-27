15a2ed109cabe82284dfe8db649831bc2b6f63fe
Jochen Panjaer <jochen.panjaer@telenet.be>
2018-06-28 00:55:23 +0200

    added Heroku keep alive

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const http = require('http');
+
 const express = require('express');
 const app = express();
 
@@ -9,3 +11,6 @@ app.use('/', express.static('client/build'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server is up and running...'));
+
+const herokuKeepAliveTime = 1800000; // 30min * 60sec * 1000ms
+setInterval(() => http.get('http://www.google.be'), herokuKeepAliveTime);
