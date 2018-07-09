e4a39d43724c2a1dbbb77fc5be21dc779d753014
Jochen Panjaer <jochen.panjaer@pandora.be>
2018-07-09 14:26:31 +0200

    installed and implemented scrolltoelement

--- client/src/react/components/hero.js
+++ client/src/react/components/hero.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import scrollToElement from 'scroll-to-element';
 
-const Hero = () => {
-    return (
-        <section className = "hero" >
-            <div className="hero__description">
-                <h1>Designer, Developer</h1>
-                <p>Hi, I'm Jochen, a Belgian freelance developer.</p>
-                <p>I make graphics and websites for small businesses.</p>
-            </div>
-            <div className="caretDown">
-                <i className="im im-angle-down" />
-            </div>
-        </section >
-    );
+class Hero extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.scrollDown = this.scrollDown.bind(this);
+    }
+
+    scrollDown(){
+        scrollToElement(
+            ".services", {   
+                offset: 0, 
+                ease: 
+                "in-out-circ", 
+                duration: 1200 
+            });
+    }
+
+    render() {
+        return (
+            <section className = "hero" >
+                <div className="hero__description">
+                    <h1>Designer, Developer</h1>
+                    <p>Hi, I'm Jochen, a Belgian freelance developer.</p>
+                    <p>I make graphics and websites for small businesses.</p>
+                </div>
+                <div className="caretDown">
+                    <i onClick={this.scrollDown} className="im im-angle-down" />
+                </div>
+            </section >
+        );
+    }
 }
 
 export default Hero;
--- index.js
+++ index.js
@@ -12,10 +12,11 @@ app.use('/', express.static('client/build'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server is up and running...'));
 
-const herokuKeepAliveTime = 1800000; // 30min * 60sec * 1000ms
+
 
 try {
   console.log("keeping alive...");
+  const herokuKeepAliveTime = 1800000; // 30min * 60sec * 1000ms
   setInterval(() => http.get("http:/http://jochenpanjaergraphics.herokuapp.com"), herokuKeepAliveTime);
 } catch(exc) {
   console.log(exc.message);
