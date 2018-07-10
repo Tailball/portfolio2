c6c61b5e84c17e72a478f6a3489ce4422ef6b57e
Jochen Panjaer <jochen.panjaer@telenet.be>
2018-07-10 14:56:11 +0200

    added logging for SG

--- routes/contact.js
+++ routes/contact.js
@@ -2,12 +2,14 @@ const express = require('express');
 const sendgrid = require('@sendgrid/mail');
 const router = express.Router();
 
-const sendGridAPI = process.env.SG_API;
 
 router.post('/', (req, res) => {
+  const sendGridAPI = process.env.SG_API;
+  
   const name = req.body.name;
   const email = req.body.email;
   const question = req.body.question;
+  console.log(`using SG API: ${sendGridAPI}`);
   console.log(`message to contact: ${name} ${email}`);
 
   //todo verification
@@ -21,11 +23,12 @@ router.post('/', (req, res) => {
   };
   sendgrid.send(msg)
   .then(sgRes => {
+    console.log(`SUCCESS from SENDGRID: ${sgRes}`);
     res.status(200).send('OK');
   })
   .catch(err => {
-    console.log(err);
-    res.status(500).send('Something went wrong...');
+    console.log(`ERROR from SENDGRID: ${err.code} - ${err.message}`);
+    res.status(500).send('Something went wrong on the server...');
   });
 });
 
