a9388157c7915d9aae6a9ef772e36f89f4654593
Jochen <jochen.panjaer@ziango.com>
2018-08-06 21:19:51 +0200

    added businesscards to portfolio

--- client/src/react/components/portfolio.js
+++ client/src/react/components/portfolio.js
@@ -6,44 +6,47 @@ import { connect } from 'react-redux';
 import moodwood from '../../styles/img/1_moodwood.jpg';
 import moodwoodfull from "../../styles/img/1_moodwood_full.jpg";
 
-import rumspringa from "../../styles/img/2_rumspringa.jpg";
-import rumspringafull from "../../styles/img/2_rumspringa_full.jpg";
+import rumspringa from '../../styles/img/2_rumspringa.jpg';
+import rumspringafull from '../../styles/img/2_rumspringa_full.jpg';
 
-import punklives from "../../styles/img/3_punklives.jpg";
-import punklivesfull from "../../styles/img/3_punklives_full.jpg";
+import punklives from '../../styles/img/3_punklives.jpg';
+import punklivesfull from '../../styles/img/3_punklives_full.jpg';
 
-import punkrocknight from "../../styles/img/4_punkrocknight.jpg";
-import punkrocknightfull from "../../styles/img/4_punkrocknight_full.jpg";
+import punkrocknight from '../../styles/img/4_punkrocknight.jpg';
+import punkrocknightfull from '../../styles/img/4_punkrocknight_full.jpg';
 
-import cremekarfest from "../../styles/img/5_cremekarfest.jpg";
-import cremekarfestfull from "../../styles/img/5_cremekarfest_full.jpg";
+import cremekarfest from '../../styles/img/5_cremekarfest.jpg';
+import cremekarfestfull from '../../styles/img/5_cremekarfest_full.jpg';
 
-import daily from "../../styles/img/5_daily.jpg";
-import dailyfull from "../../styles/img/5_daily_full.jpg";
+import daily from '../../styles/img/5_daily.jpg';
+import dailyfull from '../../styles/img/5_daily_full.jpg';
 
-import punkrockbrawl from "../../styles/img/6_punkrockbrawl.jpg";
-import punkrockbrawlfull from "../../styles/img/6_punkrockbrawl_full.jpg";
+import punkrockbrawl from '../../styles/img/6_punkrockbrawl.jpg';
+import punkrockbrawlfull from '../../styles/img/6_punkrockbrawl_full.jpg';
 
-import pennywasright from "../../styles/img/7_pennywasright.jpg";
-import pennywasrightfull from "../../styles/img/7_pennywasright_full.jpg";
+import pennywasright from '../../styles/img/7_pennywasright.jpg';
+import pennywasrightfull from '../../styles/img/7_pennywasright_full.jpg';
 
-import elephant from "../../styles/img/8_elephant.jpg";
-import elephantfull from "../../styles/img/8_elephant_full.jpg";
+import elephant from '../../styles/img/8_elephant.jpg';
+import elephantfull from '../../styles/img/8_elephant_full.jpg';
 
-import foriam from "../../styles/img/9_foriam.jpg";
-import foriamfull from "../../styles/img/9_foriam_full.jpg";
+import foriam from '../../styles/img/9_foriam.jpg';
+import foriamfull from '../../styles/img/9_foriam_full.jpg';
 
-import punkrocknight2 from "../../styles/img/10_punkrocknight.jpg";
-import punkrocknight2full from "../../styles/img/10_punkrocknight_full.jpg";
+import punkrocknight2 from '../../styles/img/10_punkrocknight.jpg';
+import punkrocknight2full from '../../styles/img/10_punkrocknight_full.jpg';
 
-import portfolio from "../../styles/img/11_portfolio.jpg";
-import portfoliofull from "../../styles/img/11_portfolio_full.jpg";
+import portfolio from '../../styles/img/11_portfolio.jpg';
+import portfoliofull from '../../styles/img/11_portfolio_full.jpg';
 
-import oolf from "../../styles/img/12_oolf.jpg";
-import oolffull from "../../styles/img/12_oolf_full.jpg";
+import oolf from '../../styles/img/12_oolf.jpg';
+import oolffull from '../../styles/img/12_oolf_full.jpg';
 
-import atn from "../../styles/img/13_atn.jpg";
-import atnfull from "../../styles/img/13_atn_full.jpg";
+import atn from '../../styles/img/13_atn.jpg';
+import atnfull from '../../styles/img/13_atn_full.jpg';
+
+import businesscards from '../../styles/img/14_businesscards.jpg';
+import businesscardsfull from '../../styles/img/14_businesscards_full.jpg';
 
 
 class Portfolio extends React.Component {
@@ -55,7 +58,7 @@ class Portfolio extends React.Component {
             lightboxIsOpen: false,
             portfoliopics: [
                 { src: punklives, width: 2, height: 3 },
-                { src: oolf, width: 1, height: 1 },
+                { src: businesscards, width: 1, height: 1 },
                 { src: elephant, width: 2, height: 3 },
                 { src: punkrockbrawl, width: 2, height: 1 },
                 { src: rumspringa, width: 1, height: 1 },
@@ -64,14 +67,15 @@ class Portfolio extends React.Component {
                 { src: pennywasright, width: 1, height: 1 },
                 { src: moodwood, width: 1, height: 1 },
                 { src: foriam, width: 2, height: 3 },
-                { src: punkrocknight, width: 2, height: 1 },
                 { src: daily, width: 1, height: 1 },
+                { src: oolf, width: 1, height: 1 },
+                { src: punkrocknight, width: 2, height: 1 },
                 { src: atn, width: 1, height: 1 },
-                { src: portfolio, width: 2, height: 1 },
+                { src: portfolio, width: 1, height: 1 },
             ],
             lightboxpics: [
                 { src: punklivesfull },
-                { src: oolffull },
+                { src: businesscardsfull },
                 { src: elephantfull },
                 { src: punkrockbrawlfull },
                 { src: rumspringafull },
@@ -80,8 +84,9 @@ class Portfolio extends React.Component {
                 { src: pennywasrightfull },
                 { src: moodwoodfull },               
                 { src: foriamfull },
-                { src: punkrocknightfull },
                 { src: dailyfull },
+                { src: oolffull },
+                { src: punkrocknightfull },
                 { src: atnfull },
                 { src: portfoliofull },
             ]         
